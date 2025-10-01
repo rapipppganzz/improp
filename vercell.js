39d632dbdb9983580c609b35ca0ed5267a6700bb
rapipppganzz <138746476+rapipppganzz@users.noreply.github.com>
2025-10-01 20:26:51 +0700

    Create vercell.js

---
+++ vercell.js
@@ -0,0 +1,12 @@
+import fetch from 'node-fetch';
+
+export default async function handler(req, res) {
+  const url = `http://depay.lekowl.shop:5005${req.url}`;
+  const response = await fetch(url, {
+    method: req.method,
+    headers: req.headers,
+    body: req.method !== 'GET' ? req.body : undefined,
+  });
+  const data = await response.text();
+  res.status(response.status).send(data);
+}
